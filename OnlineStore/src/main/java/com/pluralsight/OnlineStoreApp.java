b8258805482d83bad435593924463ffcc80098fd
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-04-25 14:24:46 -0400

    Online Store

---
+++ OnlineStore/src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class OnlineStoreApp {
+    public static void main(String[] args) {
+        HashMap<Integer, Product> products = getProduct();
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("---Welcome to the Inventory Management System---\n");
+
+        while (true) {
+            System.out.println("Please choose from the following options\n");
+            System.out.println("1- List all products\n" +
+                    "2- Lookup a product\n" +
+                    "3- Add a product\n" +
+                    "4- Remove a product");
+            System.out.print("Enter Selection: ");
+
+            int selection = keyboard.nextInt();
+            keyboard.nextLine(); // consume CRLF
+
+            switch (selection) {
+                case 1:
+                    System.out.println("\n--- Product List ---");
+                    for (Product p : products.values()) {
+                        System.out.println(p);
+                    }
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Option not implemented yet.\n");
+                    break;
+            }
+        }
+    }
+
+    public static HashMap<Integer, Product> getProduct() {
+        HashMap<Integer, Product> product = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/products.csv"))) {
+            String file;
+            while ((file = br.readLine()) != null) {
+                String[] parts = file.split("\\|");
+
+                double price = Double.parseDouble(parts[2]);
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+        return product;
+    }
+}
+
---
+++ OnlineStore/src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Product {
+    private String sku;
+    private String name;
+    private double price;
+    private String department;
+
+    public Product(String sku, String name, double price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String toString() {
+        return sku + "|" + name + "| $" + String.format("%2f" , price) + "|" + department;
+    }
+}
